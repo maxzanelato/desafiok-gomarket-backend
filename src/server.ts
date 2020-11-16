c3b080b6581466016e4dee9fcb047c6ad81d17c5
Max Zanelato <maxzanelato@gmail.com>
2020-11-16 11:03:34 -0300

    Adição de v1 no files

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/v1/files', express.static(uploadConfig.directory));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
 
